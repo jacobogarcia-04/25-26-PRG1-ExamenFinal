dc3313f87fc4b56be8435dd76d35aaaf6cb5f64d
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-01-09 15:13:42 +0100

    se deja el nombre con la primera mayuscula estaba bien

--- entregas/garciaJacobo/juegoAdivinanza.java
+++ entregas/garciaJacobo/juegoAdivinanza.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class juegoAdivinanza {
+public class JuegoAdivinanza {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
